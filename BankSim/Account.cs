d261afc460d2050292a2f781f92e93bdd6a791bf
LironSaedi <lionsaedi@gmail.com>
2017-08-30 17:36:30 -0700

    Initial Commit

---
+++ BankSim/Account.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSim
+{
+    class Account
+    {
+        //variables: username, password, money
+
+
+        //functions: constructor, withdrawl, deposit
+        //public Accont(username)
+     //   {
+            string username;
+            string password;
+            int money;
+       // }
+    }
+}
